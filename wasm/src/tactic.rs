5441668d85ba453b73788bea542a45f827b62140
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-06-24 03:16:52 +0900

    wip

--- wasm/src/tactic.rs
+++ wasm/src/tactic.rs
@@ -1,5 +1,5 @@
 #![allow(non_snake_case)]
-use crate::lang::{Formula, Sequent};
+use crate::lang::{Formula, Sequent, Term};
 use serde::{Deserialize, Serialize};
 use tsify::Tsify;
 use wasm_bindgen::prelude::*;
@@ -34,4 +34,49 @@ pub enum Result {
     Subgoal(Sequent),
     Subgoals((Sequent, Sequent)),
     Candidates(Vec<Formula>),
+}
+
+#[wasm_bindgen]
+pub fn apply(
+    tactic: Tactic,
+    mut sequent: Sequent,
+    fml1: Option<Formula>,
+    fml2: Option<Formula>,
+    term: Option<Term>,
+) -> Result {
+    use Formula::*;
+    use Tactic::*;
+    match tactic {
+        Assumption => Result::Done,
+        Intro => {
+            let fml = sequent.suc;
+            match fml {
+                Not(p) => {
+                    sequent.ant.push(*p);
+                    sequent.suc = False;
+                    Result::Subgoal(sequent)
+                }
+                To(p, q) => {
+                    sequent.ant.push(*p);
+                    sequent.suc = *q;
+                    Result::Subgoal(sequent)
+                }
+                All(x, p) => {
+                    // TODO: 2024/06/24
+                    todo!()
+                }
+                _ => unreachable!("inappropriate formula"),
+            }
+        }
+        Apply => todo!(),
+        Have => todo!(),
+        Constructor => todo!(),
+        Cases => todo!(),
+        Left => todo!(),
+        Right => todo!(),
+        Use => todo!(),
+        Trivial => todo!(),
+        Exfalso => todo!(),
+        ByContra => todo!(),
+    }
 }

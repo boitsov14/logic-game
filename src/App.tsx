4d9513adb122142701a3a70af876b8e4fc75a130
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-07-17 23:23:46 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -30,23 +30,23 @@ const App = () => {
   })
 
   return (
-    <>
-      <div class='sticky top-0 z-10 bg-neutral-900 p-2'>
+    <div class='flex min-h-screen flex-col'>
+      <header class='sticky top-0 z-10 bg-neutral-900 p-2'>
         <h1 class='text-3xl font-bold'>Logic Game</h1>
         <div class='container mx-auto px-2'>
           <GoalTab />
         </div>
-      </div>
-      <div class='container mx-auto px-4'>
-        <h1 class='pb-2 text-2xl font-bold'>Premises</h1>
+      </header>
+      <main class='container mx-auto grow px-4'>
+        <h2 class='pb-2 text-2xl font-bold'>Premises</h2>
         <Premises />
-        <h1 class='py-2 text-2xl font-bold'>Conclusion</h1>
+        <h2 class='py-2 text-2xl font-bold'>Conclusion</h2>
         <Conclusion />
-      </div>
-      <div class='container sticky bottom-0 z-10 mx-auto bg-neutral-900 p-2'>
+      </main>
+      <footer class='container sticky bottom-0 z-10 mx-auto bg-neutral-900 p-2'>
         <TacticButtons />
-      </div>
-    </>
+      </footer>
+    </div>
   )
 }
 

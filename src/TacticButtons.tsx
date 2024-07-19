a86410ce3b376ff23db9168d506f415beab2fb84
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2024-07-19 23:02:18 +0900

    wip

--- src/TacticButtons.tsx
+++ src/TacticButtons.tsx
@@ -1,9 +1,71 @@
-import { For, Show } from 'solid-js'
+import { snakeCase } from 'change-case'
+import { Component, For, Show } from 'solid-js'
 import { $enum } from 'ts-enum-util'
 import { Tactic } from 'wasm'
-import LockedTacticButton from './LockedTacticButton'
 import logic from './LogicProps'
-import TacticButton from './TacticButton'
+
+const TacticButtonAvailable: Component<{ tactic: Tactic }> = (props) => {
+  return (
+    <button
+      class='w-full rounded-full bg-gradient-to-b from-neutral-800 to-neutral-950 py-2 active:from-neutral-950 active:to-neutral-800'
+      onClick={() => logic.setTactic(props.tactic)}
+    >
+      {snakeCase(Tactic[props.tactic])}
+    </button>
+  )
+}
+
+const TacticButtonSelected: Component<{ tactic: Tactic }> = (props) => {
+  return (
+    <button class='w-full rounded-full bg-green-700 py-2' disabled>
+      {snakeCase(Tactic[props.tactic])}
+    </button>
+  )
+}
+
+const TacticButtonDisabled: Component<{ tactic: Tactic }> = (props) => {
+  return (
+    <button
+      class='w-full rounded-full bg-neutral-800 py-2 text-neutral-500'
+      disabled
+    >
+      {snakeCase(Tactic[props.tactic])}
+    </button>
+  )
+}
+
+const TacticButtonNoneSelected: Component<{ tactic: Tactic }> = (props) => {
+  return (
+    <Show
+      when={logic.tactics().includes(props.tactic)}
+      fallback={<TacticButtonDisabled tactic={props.tactic} />}
+    >
+      <TacticButtonAvailable tactic={props.tactic} />
+    </Show>
+  )
+}
+
+const TacticButtonSomeSelected: Component<{ tactic: Tactic }> = (props) => {
+  return (
+    <Show
+      when={props.tactic === logic.tactic()}
+      fallback={<TacticButtonDisabled tactic={props.tactic} />}
+    >
+      <TacticButtonSelected tactic={props.tactic} />
+    </Show>
+  )
+}
+
+const TacticButton: Component<{ tactic: Tactic }> = (props) => {
+  return (
+    <Show
+      when={logic.tactic() === null}
+      fallback={<TacticButtonSomeSelected tactic={props.tactic} />}
+    >
+      <TacticButtonNoneSelected tactic={props.tactic} />
+    </Show>
+  )
+}
 
 const TacticButtons = () => {
   return (
@@ -11,12 +73,7 @@ const TacticButtons = () => {
       <For each={$enum(Tactic).getValues()}>
         {(tactic) => (
           <div class='p-1'>
-            <Show
-              when={logic.tactic() === null}
-              fallback={<LockedTacticButton tactic={tactic} />}
-            >
-              <TacticButton tactic={tactic} />
-            </Show>
+            <TacticButton tactic={tactic} />
           </div>
         )}
       </For>
